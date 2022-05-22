b80862646f4910da08c0bcfdbfd8ed2de5963756
rcklos <2362819691@qq.com>
2022-05-22 14:33:18 +0800

    更新: 搜索栏效果

--- start-page-app/src/components/SearchBar.vue
+++ start-page-app/src/components/SearchBar.vue
@@ -12,17 +12,22 @@
 .search-bar {
   padding-bottom: 67px;
   .search-input {
+    input:hover, input:focus {
+      width: 470px;
+    }
     input {
       border: none !important;
       outline: none !important;
-      background-color: #323232 !important;
+      background-color: #C0C0C0 !important;
       display: inline-block !important;
-      width: 470px;
+      width: 127px;
       height: 47px;
       border-radius: 30px;
       padding: 2px 30px;
+      padding-right: 47px;
       font-size: 1.2rem;
       color: #ffffff !important;
+      transition: all .2s ease-in-out;
     }
   }
 }
